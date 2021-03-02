1cdee9a4b5293bfadc4b834cb3282d753bc0b0eb
awmelia <amelia.97@hotmail.se>
2021-03-02 12:18:22 +0100

    Update Program.cs

--- TUWebService/ConsoleApp1/Program.cs
+++ TUWebService/ConsoleApp1/Program.cs
@@ -12,7 +12,60 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            
+            WebService1 proxy = new WebService1();
+            string fileContent = "";
+            string anotherFile = "";
+
+            try
+            {
+                do
+                {
+                    do
+                    {
+                        Console.WriteLine("Please choose txt-file to be printed: ");
+                        string filename = Console.ReadLine();
+
+                        fileContent = proxy.ReadPath(filename);
+                        Console.WriteLine("\n" + fileContent + "\n");
+
+                    } while (fileContent == "A file with the inserted filename above does not exist. Did you forget .txt?");
+
+                } while (fileContent == "Please type filename.");
+
+
+                do
+                {
+                    Console.WriteLine("Do you want to read another txt-file?(Write yes/Yes) ");
+                    anotherFile = Console.ReadLine();
+
+                    if (anotherFile.Equals("Yes") || anotherFile.Equals("yes"))
+                    {
+                        do
+                        {
+                            do
+                            {
+                                Console.WriteLine("Please choose txt-file to be printed: ");
+                                string filename = Console.ReadLine();
+
+                                fileContent = proxy.ReadPath(filename);
+                                Console.WriteLine("\n" + fileContent + "\n");
+                            } while (fileContent == "A file with the inserted filename above does not exist. Did you forget .txt?");
+
+                        } while (fileContent == "Please type filename.");
+
+                    }
+
+                } while (anotherFile.Equals("Yes") || anotherFile.Equals("yes"));
+
+            }
+            catch (WebException)
+            {
+                Console.WriteLine("\nPlease check your connection and try again.");
+            }
+
+            Console.WriteLine("\n---END OF FILE READING---");
+            Console.ReadLine();
+
         }
     }
 }
